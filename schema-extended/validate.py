71d348e14bb70ff0a7dd2e2fd4ac7ae6ec9a30a8
Martin Lessmeister <mhl10@cornell.edu>
2021-11-11 10:55:03 -0500

    add `native_names` to authors; `size_kilobytes` -> `size_bytes`; remove outdated docs

--- schema-extended/validate.py
+++ schema-extended/validate.py
@@ -1,3 +1,4 @@
+"""Jsonschema validation script that supports file $refs."""
 import click
 import json
 from jsonschema import RefResolver, Draft7Validator
@@ -13,31 +14,40 @@ schema_files = [
     "Keyword.json",
     "LanguageCode.json",
     "License.json",
-    "Person.json",
+    "Author.json",
     "References.json",
     "RelatedLink.json",
     "Role.json",
     "Taxonomy.json",
     "Version.json"
 ]
-for sf in schema_files:
-    schema = json.loads(open(sf).read())
-    schema_store[schema.get("$id", sf)] = schema
-
-document = json.loads(open("Document.json").read())
-resolver = RefResolver.from_schema(document, store=schema_store)
-Validator = validator_for(document)
-validator = Validator(document, resolver=resolver)
-
+try:
+    for sf in schema_files:
+        schema = json.loads(open(sf).read())
+        schema_store[schema.get("$id", sf)] = schema
+except Exception:
+    print(f"Could not load {sf}")
 
 @click.command()
+@click.option(
+    "--validation_schema",
+    "-s",
+    type=click.Path(exists=True),
+    help="schema to validate against",
+    default="Document.json"
+)
 @click.option(
     "--instance",
     "-i",
     type=click.Path(exists=True),
     help="path to instance json",
 )
-def main(instance: str) -> None:
+def main(instance: str, validation_schema: str) -> None:
+    schema_json = json.loads(open(validation_schema).read())
+    resolver = RefResolver.from_schema(schema_json, store=schema_store)
+    Validator = validator_for(schema_json)
+    validator = Validator(schema_json, resolver=resolver)
+
     with open(instance) as instance_file:
         instance_json = json.loads(instance_file.read())
     validator.validate(instance_json)
